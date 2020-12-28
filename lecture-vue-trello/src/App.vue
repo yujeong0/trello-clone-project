de638a5b7534ceb8ec74764683705419879a212c
yujeong0 <kos9811@naver.com>
2020-12-28 21:12:10 +0900

    라우터 링크 완료

--- lecture-vue-trello/src/App.vue
+++ lecture-vue-trello/src/App.vue
@@ -1,13 +1,15 @@
 <template>
   <div id="app">
-    여기부터 시작
+    <navbar></navbar>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import Navbar from './components/Navbar.vue'
 export default {
   name: 'app',
+  components: { Navbar },
   data () {
     return {
     }
---
+++ lecture-vue-trello/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <router-link to="/">Home</router-link>
+        <router-link to="/login">Login</router-link>  <!-- router-link 를 사용하여 페이지 재로딩 방지 -->
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
