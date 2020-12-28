9c133a47ae7aaa4087e607096a49427db66c4c44
yujeong0 <kos9811@naver.com>
2020-12-28 17:19:51 +0900

    vue-router 기본 설정

--- lecture-vue-trello/src/main.js
+++ lecture-vue-trello/src/main.js
@@ -1,22 +1,23 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
 
 const Login = { template: '<div>Login Page</div>' }
+const NotFound = { template: '<div>Page Not Found</div>' }
 
-const routes = {
-  '/': App,
-  '/login': Login
-}
+const router = new VueRouter({
+  mode: 'history',
+  routes: [
+    { path: '/', component: App },
+    { path: '/login', component: Login },
+    { path: '*', component: NotFound }
+  ]
+})
 
 new Vue({
   el: '#app',
-  computed: {
-    Vuecomponent(){
-      return routes[window.location.pathname] ||
-        { template: '<div> Page Not Found </div>' } 
-    }
-  },
-  render(h) {
-    return h(this.Vuecomponent)
-  }
+  router,
+  render: h => h({template: '<router-view></router-view>'})
 })
