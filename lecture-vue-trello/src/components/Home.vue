7b13ece25118982ce11fd9f1ea3dc26da297ac3f
yujeong0 <kos9811@naver.com>
2021-01-04 21:58:50 +0900

    보드 조회 API 연동

--- lecture-vue-trello/src/components/Home.vue
+++ lecture-vue-trello/src/components/Home.vue
@@ -5,9 +5,10 @@
             Board List :
             <div v-if="loading">Loading...</div>
             <div v-else>
-                Api result: <pre>{{apiRes}}</pre>
+                <div v-for="b in boards" :key="b.id">
+                    {{b}}
+                </div>
             </div>
-            <div v-if="error"><pre>{{error}}</pre></div>
             <ul>
                 <li>
                     <router-link to="/b/1">Board 1</router-link>
@@ -27,8 +28,7 @@ export default {
     data() {
         return {
             loading: false,
-            apiRes: '',
-            error: ''
+            boards: [],
         }
     },
     created() {
@@ -37,12 +37,12 @@ export default {
     methods: {
         fetchData() {
             this.loading = true
-            axios.get('http://localhost:3000/health')
+            axios.get('http://localhost:3000/boards')
                 .then(res => {
-                    this.apiRes = res.data
+                    this.boards = res.data
                 })
                 .catch(res => {
-                    this.error = res.response.data
+                    this.$router.replace('/login') 
                 })
                 .finally(() => {
                     this.loading = false
