8e0b9b10206660d8f62b8e4c944f5b7ef9141945
yujeong0 <kos9811@naver.com>
2021-01-04 23:22:24 +0900

    로그인 + 로그인  api 연동

--- lecture-vue-trello/src/api/index.js
+++ lecture-vue-trello/src/api/index.js
@@ -4,7 +4,7 @@ import router from '../router'
 const DOMAIN = 'http://localhost:3000'
 const UNAUTHORIZED = 401
 const onUnauthorized = () => {
-    router.push('/login')
+    router.push(`/login?rPath=${encodeURIComponent(location.pathname)}`)
 }
 
 const request = (method, url, data) => {
@@ -15,13 +15,23 @@ const request = (method, url, data) => {
     }).then(result => result.data)
         .catch(result => {
             const { status } = result.response
-            if (status === UNAUTHORIZED) return onUnauthorized()
-            throw Error(result)
+            if (status === UNAUTHORIZED) onUnauthorized()
+            throw result.response
       })
+}
+
+export const setAuthInHeader = token => {
+    axios.defaults.headers.common['Authorization'] = token ? `Bearer ${token}` : null;
 }
 
 export const board = {
     fetch() {
         return request('get', '/boards')
+    }
+}
+
+export const auth = {
+    login(email, password) {
+        return request('post', '/login', {email, password})
     }
 }
