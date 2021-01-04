03b9d4b78996d68f18cafe4a3f705b5d39dc461f
yujeong0 <kos9811@naver.com>
2021-01-04 23:48:00 +0900

    로그아웃

--- lecture-vue-trello/src/components/Navbar.vue
+++ lecture-vue-trello/src/components/Navbar.vue
@@ -1,16 +1,75 @@
 <template>
-    <div>
-        <router-link to="/">Home</router-link>
-        <router-link to="/login">Login</router-link>  <!-- router-link 를 사용하여 페이지 재로딩 방지 -->
-    </div>
+    <nav class="header">
+        <div class="header-logo">
+            <router-link to="/">Home</router-link>
+        </div>
+        <div class="header-auth">
+            <a href="" v-if="isAuth" @click.prevent="logout">Logout</a>
+            <router-link v-else to="/login">Login</router-link> <!-- router-link 를 사용하여 페이지 재로딩 방지 -->
+        </div>
+    </nav>
 </template>
 
 <script>
+import { setAuthInHeader } from '../api'
+
 export default {
-    
+    computed: {
+        isAuth() {
+            return !!localStorage.getItem('token')  // boolean 으로 바꾼 거..
+        }
+    },
+    methods: {
+        logout() {
+            delete localStorage.token
+            setAuthInHeader(null)   // http 헤더값 초기화
+            this.$router.push('/login')
+        }
+    }
 }
 </script>
 
 <style>
-
+.header {
+  flex: none;
+  background-color: rgba(0,0,0,.15);
+  height: 32px;
+  padding: 4px;
+}
+.header a {
+  display: block;
+  height: 30px;
+  line-height: 30px;
+  text-decoration: none;
+  color: rgba(255,255,255,.5);
+}
+.header-logo {
+  position: absolute;
+  left: 50%;
+  top: 7px;
+  margin-left: -30px;
+  text-align: center;
+  font-weight: bolder;
+  font-size: 24px;
+}
+.header-logo a:hover,
+.header-logo a:focus {
+  color: rgba(255,255,255,.9);
+}
+.header-auth {
+  position: absolute;
+  right: 15px;
+  top: 5px;
+}
+.header-auth a {
+  border-radius: 2px;
+  padding: 0 10px;
+  background-color: rgba(255,255,255, .5);
+  color: white;
+  transition: all .3s;
+}
+.header-auth a:hover,
+.header-auth a:focus {
+  background-color: rgba(255,255,255, .3);
+}
 </style>
