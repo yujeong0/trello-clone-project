1aa1352f6c3b85b262e469e16510077c92443a26
강유정 <kos9811@naver.com>
2021-02-24 15:14:16 +0900

    vuex 인증1

--- lecture-vue-trello/src/store/index.js
+++ lecture-vue-trello/src/store/index.js
@@ -7,7 +7,13 @@ Vue.use(Vuex)
 const store = new Vuex.Store({
     state: {
         isAddBoard: false,
-        boards: []
+        boards: [],
+        token: null
+    },
+    getters: {
+        isAuth(state) { // 로그인 여부 확인
+            return !!state.token
+        }
     },
     mutations: {
         SET_IS_ADD_BOARD (state, toggle) {
@@ -15,9 +21,20 @@ const store = new Vuex.Store({
         },
         SET_BOARDS (state, boards) {
             state.boards = boards;
+        },
+        LOGIN(state, token) {
+            if(!token) return
+            state.token = token
+            localStorage.setItem('token', token)
+            api.setAuthInHeader(token)
+        },
+        LOGOUT(state) {
+            state.token = null
+            delete localStorage.token
+            api.setAuthInHeader(null)
         }
     },
-    actions: {
+    actions: {  // 비동기
         ADD_BOARD(_, {title}) {
             return api.board.create(title)
         },
@@ -25,8 +42,15 @@ const store = new Vuex.Store({
             return api.board.fetch().then(data => {
                 commit('SET_BOARDS', data.list)
             })
+        },
+        LOGIN({commit}, {email, password}) {    // api call 을 위한 login
+            return api.auth.login(email, password)  
+              .then(({accessToken}) => commit('LOGIN', accessToken))
         }
     }
 })
+
+const { token } = localStorage
+store.commit('LOGIN', token)
 
 export default store
